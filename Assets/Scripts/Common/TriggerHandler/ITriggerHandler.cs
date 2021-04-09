9a8359d0374bf0b854cc0facd6013342e27be97c
Ferbdi <murashkin2002@gmail.com>
2021-04-09 09:32:15 +0600

    Create finish trigger

---
+++ Assets/Scripts/Common/TriggerHandler/ITriggerHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+namespace BlissfulMaze.Common
+{
+    public interface ITriggerHandler
+    {
+        event Action<Collider> Enter;
+        event Action<Collider> Stay;
+        event Action<Collider> Exit;
+        bool HasInside(GameObject go);
+    }
+}
---
+++ Assets/Scripts/Common/TriggerHandler/TriggerHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace BlissfulMaze.Common
+{
+    [RequireComponent(typeof(Collider))]
+    public class TriggerHandler : MonoBehaviour, ITriggerHandler
+    {
+        public event Action<Collider> Enter;
+        public event Action<Collider> Stay;
+        public event Action<Collider> Exit;
+
+        private List<GameObject> _objectsInside = new List<GameObject>();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            _objectsInside.Add(other.gameObject);
+            Enter?.Invoke(other);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (!_objectsInside.Contains(other.gameObject))
+                _objectsInside.Add(other.gameObject);
+            Stay?.Invoke(other);
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            _objectsInside.Remove(other.gameObject);
+            Exit?.Invoke(other);
+        }
+
+        public bool HasInside(GameObject go) => _objectsInside.Contains(go);
+    }
+}
