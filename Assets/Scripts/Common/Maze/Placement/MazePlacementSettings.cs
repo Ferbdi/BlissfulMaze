ed1aec5d5392905755afe0a07119842c3f3b6d1f
Ferbdi <murashkin2002@gmail.com>
2021-04-12 10:32:38 +0600

    Create maze placement settings
    Create scriptable object

---
+++ Assets/Scripts/Common/Maze/Placement/MazePlacementSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BlissfulMaze.Common.Maze
+{
+    [CreateAssetMenu(fileName = "Maze Placement Settings", menuName = "ScriptableObjects/Maze/Placement Settings", order = 0)]
+    public class MazePlacementSettings : ScriptableObject
+    {
+        [SerializeField] private GameObject _mazeCellPrefab;
+        [SerializeField] private GameObject _mazeFinishTriggerPrefab;
+        [SerializeField] private AnimationCurve _placementCurve;
+        [SerializeField] private float _speedOfPlacementUp;
+
+        public GameObject MazeCellPrefab => _mazeCellPrefab;
+        public GameObject MazeFinishTriggerPrefab => _mazeFinishTriggerPrefab;
+        public AnimationCurve PlacementCurve => _placementCurve;
+        public float SpeedOfPlacementUp => _speedOfPlacementUp;
+    }
+}
