dad68355aa85f4f3afd0d2b95bcce4567e1ae722
Ferbdi <murashkin2002@gmail.com>
2021-04-14 08:17:11 +0600

    Create simple logic for game manager

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,12 +1,36 @@
 ﻿using UnityEngine;
+using Zenject;
+using BlissfulMaze.Common;
+using BlissfulMaze.Common.Maze;
+using UnityEngine.SceneManagement;
 
 namespace BlissfulMaze.Core
 {
     public class GameManager : MonoBehaviour, IGameManager
     {
+        private Player _player;
+        private MazeBehaviour _mazeBehaviour;
+
+        [Inject]
+        private void Construct(Player player, MazeBehaviour mazeBehaviour)
+        {
+            _player = player;
+            _mazeBehaviour = mazeBehaviour;
+        }
+
+        private void Start()
+        {
+            _mazeBehaviour.FinishTrigger.Enter += (Collider collider) =>
+            {
+                if (collider.gameObject == _player.gameObject)
+                    Debug.Log("Finish!!!");
+            };
+        }
+
         private void Update()
         {
-            Debug.Log("Hello");
+            if (Input.GetKeyDown(KeyCode.R))
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
