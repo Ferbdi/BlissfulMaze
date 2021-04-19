e453ef5565179c68d1d4c87277fa329fa8d23ed2
Ferbdi <murashkin2002@gmail.com>
2021-04-19 18:44:29 +0600

    Update maze behaviour for currently system

--- Assets/Scripts/Common/Maze/Behaviour/MazeBehaviour.cs
+++ Assets/Scripts/Common/Maze/Behaviour/MazeBehaviour.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using Zenject;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BlissfulMaze.Common.Maze
 {
@@ -12,9 +12,12 @@ namespace BlissfulMaze.Common.Maze
         [SerializeField] private int _width;
         [SerializeField] private int _height;
 
+        public MazePlacementSettings MazePlacementSettings { get => _mazePlacementSettings; }
         public ITriggerHandler FinishTrigger { get => _mazePlacementService.FinishTrigger; }
         private IMazeGenerator _mazeGenerator;
         private IMazePlacementService _mazePlacementService;
+        private bool _isRecreating;
+        private bool _finishInStartPosition;
 
         [Inject]
         private void Construct(IMazeGenerator mazeGenerator, IMazePlacementService mazePlacementService)
@@ -26,23 +29,42 @@ namespace BlissfulMaze.Common.Maze
         private void Awake()
         {
             Setup();
+            _mazePlacementService.MoveUp();
         }
 
         private void Setup()
         {
-            var maze = _mazeGenerator.Generate(_width, _height);
-            _mazePlacementService.Instantiate(maze, _mazePlacementSettings, transform);
-            //_mazePlacementService.MoveUp(cells, _mazePlacementSettings);
+            var maze = _mazeGenerator.Generate(_width, _height, _finishInStartPosition ? new Vector2Int(1, 1) : new Vector2Int(_width - 2, _height - 2));
+            _finishInStartPosition = !_finishInStartPosition;
+            _mazePlacementService.Setup(maze, _mazePlacementSettings, transform);
+        }
+
+        public async Task Recreate()
+        {
+            if (_isRecreating) return;
+            _isRecreating = true;
+
+            while (_mazePlacementService.IsMoving) { await Task.Yield(); }
+            _mazePlacementService.MoveDown();
+            while (_mazePlacementService.IsMoving) { await Task.Yield(); }
+            Setup();
+            _mazePlacementService.MoveUp();
+            while (_mazePlacementService.IsMoving) { await Task.Yield(); }
+
+            _isRecreating = false;
         }
 
         private void Update()
         {
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                if (_mazePlacementService.PlacementState == PlacementState.Up)
-                    _mazePlacementService.MoveDown(_mazePlacementSettings);
-                else
-                    _mazePlacementService.MoveUp(_mazePlacementSettings);
+                if (_mazePlacementService.PlacementState == PlacementState.Up && !_mazePlacementService.IsMoving)
+                    _mazePlacementService.MoveDown();
+                else if (_mazePlacementService.PlacementState == PlacementState.Down && !_mazePlacementService.IsMoving)
+                {
+                    Setup();
+                    _mazePlacementService.MoveUp();
+                }
             }
         }
     }
