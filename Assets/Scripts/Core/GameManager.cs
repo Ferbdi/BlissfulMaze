b03f94a55a6fb14a4b870579d2765c6d2cd55d9f
Ferbdi <murashkin2002@gmail.com>
2021-04-19 18:44:51 +0600

    Create some logic for game

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -9,22 +9,38 @@ namespace BlissfulMaze.Core
     public class GameManager : MonoBehaviour, IGameManager
     {
         private Player _player;
+        private IPlayerInputService _playerInputService;
         private MazeBehaviour _mazeBehaviour;
 
         [Inject]
-        private void Construct(Player player, MazeBehaviour mazeBehaviour)
+        private void Construct(Player player, IPlayerInputService playerInputService, MazeBehaviour mazeBehaviour)
         {
             _player = player;
+            _playerInputService = playerInputService;
             _mazeBehaviour = mazeBehaviour;
         }
 
         private void Start()
         {
-            _mazeBehaviour.FinishTrigger.Enter += (Collider collider) =>
+            _mazeBehaviour.FinishTrigger.Enter += OnFinishTriggerEnter;
+        }
+
+        private async void OnFinishTriggerEnter(Collider collider)
+        {
+            try
             {
                 if (collider.gameObject == _player.gameObject)
+                {
                     Debug.Log("Finish!!!");
-            };
+                    _playerInputService.IsEnabled = false;
+                    await _mazeBehaviour.Recreate();
+                    _playerInputService.IsEnabled = true;
+                }
+            }
+            catch
+            {
+                Debug.LogWarning("[WARNING] MazeBehaviour is missing. Maybe scene is reloaded.");
+            }
         }
 
         private void Update()
