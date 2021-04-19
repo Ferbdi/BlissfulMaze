1741e12f9822f21a70641fa6e9b432c6a6ce3f46
Ferbdi <murashkin2002@gmail.com>
2021-04-19 18:39:24 +0600

    Create maze placement cell and finish
    Create script, prefab and pool to cell and finish

---
+++ Assets/Scripts/Common/Maze/Placement/MazePlacementCell.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace BlissfulMaze.Common.Maze
+{
+    public class MazePlacementCell : MonoBehaviour
+    {
+        public class Pool : MonoMemoryPool<Vector3, MazePlacementCell>
+        {
+            protected override void Reinitialize(Vector3 position, MazePlacementCell mazePlacementCell)
+            {
+                mazePlacementCell.transform.position = position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Common/Maze/Placement/MazePlacementFinish.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zenject;
+using System.Threading.Tasks;
+
+namespace BlissfulMaze.Common.Maze
+{
+    public class MazePlacementFinish : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _particleSystem;
+
+        private async Task Despawn()
+        {
+            var main = _particleSystem.main;
+            var gravityBefore = main.gravityModifier;
+            main.gravityModifier = 2f;
+            _particleSystem.Stop();
+
+            await Task.Delay(1000);
+            main.gravityModifier = gravityBefore;
+        }
+
+        public class Pool : MonoMemoryPool<Vector3, MazePlacementFinish>
+        {
+            protected override void Reinitialize(Vector3 position, MazePlacementFinish mazePlacementFinish)
+            {
+                mazePlacementFinish.transform.position = position;
+            }
+
+            protected override async void OnDespawned(MazePlacementFinish item)
+            {
+                await item.Despawn();
+                base.OnDespawned(item);
+            }
+        }
+    }
+}
