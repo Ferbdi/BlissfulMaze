4700a6ac54eda042e9359a2efa2192e165fb374d
Ferbdi <murashkin2002@gmail.com>
2021-04-20 11:27:55 +0600

    Refactor trigger handler

--- Assets/Scripts/Common/TriggerHandler/TriggerHandler.cs
+++ Assets/Scripts/Common/TriggerHandler/TriggerHandler.cs
@@ -7,28 +7,28 @@ namespace BlissfulMaze.Common
     [RequireComponent(typeof(Collider))]
     public class TriggerHandler : MonoBehaviour, ITriggerHandler
     {
-        public event Action<Collider> Enter;
-        public event Action<Collider> Stay;
-        public event Action<Collider> Exit;
+        public event Action<Collider> OnEnter;
+        public event Action<Collider> OnStay;
+        public event Action<Collider> OnExit;
 
         private List<GameObject> _objectsInside = new List<GameObject>();
 
         private void OnTriggerEnter(Collider other)
         {
             _objectsInside.Add(other.gameObject);
-            Enter?.Invoke(other);
+            OnEnter?.Invoke(other);
         }
 
         private void OnTriggerStay(Collider other)
         {
             if (!_objectsInside.Contains(other.gameObject))
                 _objectsInside.Add(other.gameObject);
-            Stay?.Invoke(other);
+            OnStay?.Invoke(other);
         }
         private void OnTriggerExit(Collider other)
         {
             _objectsInside.Remove(other.gameObject);
-            Exit?.Invoke(other);
+            OnExit?.Invoke(other);
         }
 
         public bool HasInside(GameObject go) => _objectsInside.Contains(go);
