d4252a2fda3ab9d3d4fa63f39bccbec8b7768701
Ferbdi <murashkin2002@gmail.com>
2021-04-20 13:54:36 +0600

    Refactor simple maze generation algorithm

--- Assets/Scripts/Common/Maze/Generator/Algorithms/SimpleMazeGenerationAlgorithm.cs
+++ Assets/Scripts/Common/Maze/Generator/Algorithms/SimpleMazeGenerationAlgorithm.cs
@@ -8,15 +8,15 @@ namespace BlissfulMaze.Common.Maze
 {
     public class SimpleMazeGenerationAlgorithm : IMazeGenerationAlgorithm
     {
-        private IEnumerable<Vector2Int> GetNeighbours(IMaze maze, bool[,] visitedCells, int x, int y) // Получаем соседа текущей клетки
+        private IEnumerable<Vector2Int> GetNeighbours(IMaze maze, bool[,] visitedCells, Vector2Int position)
         {
             const int distance = 2;
-            Vector2Int[] possibleNeighbours = new[] // Список всех возможных соседeй
+            Vector2Int[] possibleNeighbours = new[]
             {
-                new Vector2Int(x, y - distance), // Up
-                new Vector2Int(x + distance, y), // Right
-                new Vector2Int(x, y + distance), // Down
-                new Vector2Int(x - distance, y) // Left
+                new Vector2Int(position.x, position.y - distance), // Up
+                new Vector2Int(position.x + distance, position.y), // Right
+                new Vector2Int(position.x, position.y + distance), // Down
+                new Vector2Int(position.x - distance, position.y) // Left
             };
 
             return possibleNeighbours.Where(neighbour =>
@@ -26,7 +26,7 @@ namespace BlissfulMaze.Common.Maze
 
         private bool IsNeighbourIsCanToVisit(IMaze maze, bool[,] visitedCells, Vector2Int neighbour)
         {
-            return maze.Cells[neighbour.x, neighbour.y].HasFlag(TypeMazeCell.Empty) && !visitedCells[neighbour.x, neighbour.y];
+            return maze.Cells[neighbour.y, neighbour.x].HasFlag(TypeMazeCell.Empty) && !visitedCells[neighbour.y, neighbour.x];
         }
 
         private bool IsNeighbourInsideMaze(IMaze maze, Vector2Int neighbour)
@@ -34,7 +34,7 @@ namespace BlissfulMaze.Common.Maze
             return neighbour.x > 0 && neighbour.x < maze.Width && neighbour.y > 0 && neighbour.y < maze.Height;
         }
 
-        private Vector2Int ChooseRandomNeighbour(IEnumerable<Vector2Int> neighbours) //выбор случайного соседа
+        private Vector2Int ChooseRandomNeighbour(IEnumerable<Vector2Int> neighbours)
         {
             int index = Random.Range(0, neighbours.Count());
             return neighbours.ElementAt(index);
@@ -44,33 +44,33 @@ namespace BlissfulMaze.Common.Maze
         {
             int xDiff = second.x - first.x;
             int yDiff = second.y - first.y;
-            int addX = (xDiff != 0) ? xDiff / Math.Abs(xDiff) : 0; // Узнаем направление удаления стены
+            int addX = (xDiff != 0) ? xDiff / Math.Abs(xDiff) : 0;
             int addY = (yDiff != 0) ? yDiff / Math.Abs(yDiff) : 0;
-            // Координаты удаленной стены
-            maze.Cells[first.x + addX, first.y + addY] = TypeMazeCell.Empty; //обращаем стену в клетку
-            visitedCells[first.x + addX, first.y + addY] = true; //и делаем ее посещенной
-            visitedCells[second.x, second.y] = true; //делаем клетку посещенной
+
+            maze.Cells[first.y + addY, first.x + addX] = TypeMazeCell.Empty;
+            visitedCells[first.y + addY, first.x + addX] = true;
+            visitedCells[second.y, second.x] = true;
         }
 
         private void GenerateMaze(IMaze maze, Vector2Int finishPosition)
         {
             bool[,] visitedCells = new bool[maze.Height, maze.Width];
-            Stack<Vector2Int> _path = new Stack<Vector2Int>();
-            _path.Push(finishPosition);
+            Stack<Vector2Int> path = new Stack<Vector2Int>();
+            path.Push(finishPosition);
 
-            while (_path.Count != 0) //пока в стеке есть клетки ищем соседей и строим путь
+            while (path.Count != 0)
             {
-                var neighbours = GetNeighbours(maze, visitedCells, _path.Peek().x, _path.Peek().y);
+                var neighbours = GetNeighbours(maze, visitedCells, path.Peek());
                 if (neighbours.Count() != 0)
                 {
                     var nextCell = ChooseRandomNeighbour(neighbours);
-                    RemoveWall(maze, ref visitedCells, _path.Peek(), nextCell);
-                    visitedCells[nextCell.x, nextCell.y] = true; //делаем текущую клетку посещенной
-                    _path.Push(nextCell); //затем добавляем её в стек
+                    RemoveWall(maze, ref visitedCells, path.Peek(), nextCell);
+                    visitedCells[nextCell.y, nextCell.x] = true;
+                    path.Push(nextCell);
                 }
                 else
                 {
-                    _path.Pop();
+                    path.Pop();
                 }
             }
         }
